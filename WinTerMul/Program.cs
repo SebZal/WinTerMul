52d37a6647fcedd5fca5669c2b69e723f6984d8f
Sebastian Zalewski <1532036+SebZal@users.noreply.github.com>
2019-03-10 23:16:12 +0100

    Implemented simple terminal with input, output, and error redirection.

--- WinTerMul/Program.cs
+++ WinTerMul/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace WinTerMul
+﻿using System;
+using System.Diagnostics;
+
+namespace WinTerMul
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
+            var process = new Process
+            {
+                StartInfo = new ProcessStartInfo("cmd.exe")
+                {
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    RedirectStandardInput = true,
+                }
+            };
+
+            process.OutputDataReceived += Process_OutputDataReceived;
+            process.ErrorDataReceived += Process_ErrorDataReceived;
+
+            process.Start();
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                process.StandardInput.WriteLine(line);
+            }
+        }
+
+        private static void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine(e.Data);
+        }
+
+        private static void Process_ErrorDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Console.WriteLine(e.Data);
         }
     }
 }
