1b3f444cfabd650ac95975e6dc2174846400d18a
Sebastian Zalewski <1532036+SebZal@users.noreply.github.com>
2019-03-23 23:00:38 +0100

    Create ResizeHandler class

--- WinTerMul/Program.cs
+++ WinTerMul/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading;
-
-using Newtonsoft.Json;
 
 using WinTerMul.Common;
 
@@ -13,124 +9,74 @@ namespace WinTerMul
     {
         private static void Main(string[] args)
         {
-            var terminalContainer = new TerminalContainer(Terminal.Create());
-
-            new Renderer(terminalContainer).StartRendererThread();
-
             var inputHandle = PInvoke.Kernel32.GetStdHandle(PInvoke.Kernel32.StdHandle.STD_INPUT_HANDLE);
             var outputHandle = PInvoke.Kernel32.GetStdHandle(PInvoke.Kernel32.StdHandle.STD_OUTPUT_HANDLE);
 
-            using (var sha1 = new SHA1CryptoServiceProvider())
+            var terminalContainer = new TerminalContainer(Terminal.Create()); // TODO dispose
+            var resizeHandler = new ResizeHandler(terminalContainer, outputHandle); // TODO dispose
+
+            new Renderer(terminalContainer).StartRendererThread();
+
+            var wasLastKeyCtrlS = false;
+
+            while (true)
             {
-                var wasLastKeyCtrlS = false;
+                Thread.Sleep(10);
 
-                var previousHash = new byte[sha1.HashSize / 8];
-                while (true)
+                var terminals = terminalContainer.GetTerminals();
+                if (terminals.Count == 0)
                 {
-                    Thread.Sleep(10);
+                    break;
+                }
 
-                    var terminals = terminalContainer.GetTerminals();
-                    if (terminals.Count == 0)
+                resizeHandler.CheckAndHandleResize();
+
+                PInvoke.Kernel32.ReadConsoleInput(inputHandle, out var lpBuffer, 1, out var n);
+                if (lpBuffer.EventType == PInvoke.Kernel32.InputEventTypeFlag.KEY_EVENT)
+                {
+                    if (wasLastKeyCtrlS && lpBuffer.Event.KeyEvent.uChar.UnicodeChar != '')
                     {
-                        break;
-                    }
+                        wasLastKeyCtrlS = false;
 
-                    if (PInvoke.Kernel32.GetConsoleScreenBufferInfo(outputHandle, out var bufferInfo))
-                    {
-                        bufferInfo.dwCursorPosition = new PInvoke.COORD(); // Ignore cursor position
-                        bufferInfo.dwMaximumWindowSize.X  = (short)(bufferInfo.dwMaximumWindowSize.X / terminals.Count);
-                        var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(bufferInfo)));
-
-                        var isHashDifferent = false;
-                        for (int i = 0; i < hash.Length; i++)
+                        switch (lpBuffer.Event.KeyEvent.uChar.UnicodeChar)
                         {
-                            if (hash[i] != previousHash[i])
-                            {
-                                isHashDifferent = true;
+                            case 's':
+                            case '':
+                            case '\0':
+                            case '\u000f':
+                                wasLastKeyCtrlS = true;
                                 break;
-                            }
+                            case 'l':
+                                terminalContainer.SetNextTerminalActive();
+                                break;
+                            case 'h':
+                                terminalContainer.SetPreviousTerminalActive();
+                                break;
+                            case 'v':
+                                terminalContainer.AddTerminal(Terminal.Create());
+                                break;
+                            //case 'h': // TODO horizontal split
+                            default:
+                                break;
                         }
 
-                        if (isHashDifferent)
-                        {
-                            previousHash = hash;
-
-                            foreach (var terminal in terminals)
-                            {
-                                try
-                                {
-                                    terminal.In.Write(new ResizeCommand
-                                    {
-                                        Width = bufferInfo.dwMaximumWindowSize.X,
-                                        Height = bufferInfo.dwMaximumWindowSize.Y
-                                    });
-                                }
-                                catch (ObjectDisposedException)
-                                {
-                                    // Process has exited, next iteration should resend correct data.
-                                    break;
-                                }
-                            }
-
-                            continue;
-                        }
-                    }
-                    else
-                    {
-                        // TODO
-                        var error = PInvoke.Kernel32.GetLastError().ToString();
-                        Console.WriteLine(error);
+                        continue;
                     }
 
-                    PInvoke.Kernel32.ReadConsoleInput(inputHandle, out var lpBuffer, 1, out var n);
-                    if (lpBuffer.EventType == PInvoke.Kernel32.InputEventTypeFlag.KEY_EVENT)
+                    if (lpBuffer.Event.KeyEvent.uChar.UnicodeChar == '') // CTRL + s
                     {
-                        if (wasLastKeyCtrlS && lpBuffer.Event.KeyEvent.uChar.UnicodeChar != '')
-                        {
-                            wasLastKeyCtrlS = false;
+                        wasLastKeyCtrlS = true;
+                        continue;
+                    }
 
-                            switch (lpBuffer.Event.KeyEvent.uChar.UnicodeChar)
-                            {
-                                case 's':
-                                case '':
-                                case '\0':
-                                case '\u000f':
-                                    wasLastKeyCtrlS = true;
-                                    break;
-                                case 'l':
-                                    terminalContainer.SetNextTerminalActive();
-                                    break;
-                                case 'h':
-                                    terminalContainer.SetPreviousTerminalActive();
-                                    break;
-                                case 'v':
-                                    terminalContainer.AddTerminal(Terminal.Create());
-                                    // Force resize
-                                    previousHash = new byte[sha1.HashSize / 8]; // TODO find a better way
-                                    break;
-                                //case 'h': // TODO horizontal split
-                                default:
-                                    break;
-                            }
-
-                            continue;
-                        }
-
-                        if (lpBuffer.Event.KeyEvent.uChar.UnicodeChar == '') // CTRL + s
-                        {
-                            wasLastKeyCtrlS = true;
-                            continue;
-                        }
-
-                        try
-                        {
-                            terminalContainer.ActiveTerminal?.In.Write(new TransferableInputRecord { InputRecord = lpBuffer });
-                        }
-                        catch (ObjectDisposedException)
-                        {
-                            // Process has exited, new active terminal will be set in next iteration.
-                            continue;
-                        }
+                    try
+                    {
+                        terminalContainer.ActiveTerminal?.In.Write(new TransferableInputRecord { InputRecord = lpBuffer });
+                    }
+                    catch (ObjectDisposedException)
+                    {
+                        // Process has exited, new active terminal will be set in next iteration.
+                        continue;
                     }
                 }
             }
---
+++ WinTerMul/ResizeHandler.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+using Newtonsoft.Json;
+
+using WinTerMul.Common;
+
+namespace WinTerMul
+{
+    internal class ResizeHandler : IDisposable
+    {
+        private readonly TerminalContainer _terminalContainer;
+        private readonly IntPtr _outputHandle;
+        private readonly SHA1CryptoServiceProvider _sha1;
+
+        private byte[] _previousHash;
+
+        public ResizeHandler(
+            TerminalContainer terminalContainer,
+            IntPtr outputHandle)
+        {
+            _terminalContainer = terminalContainer;
+            _outputHandle = outputHandle;
+            _sha1 = new SHA1CryptoServiceProvider();
+            _previousHash = new byte[_sha1.HashSize / 8];
+        }
+
+        public void CheckAndHandleResize()
+        {
+            var terminals = _terminalContainer.GetTerminals();
+
+            if (PInvoke.Kernel32.GetConsoleScreenBufferInfo(_outputHandle, out var bufferInfo))
+            {
+                bufferInfo.dwCursorPosition = new PInvoke.COORD(); // Ignore cursor position
+                bufferInfo.dwMaximumWindowSize.X = (short)(bufferInfo.dwMaximumWindowSize.X / terminals.Count);
+                var hash = _sha1.ComputeHash(Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(bufferInfo)));
+
+                var isHashDifferent = false;
+                for (int i = 0; i < hash.Length; i++)
+                {
+                    if (hash[i] != _previousHash[i])
+                    {
+                        isHashDifferent = true;
+                        break;
+                    }
+                }
+
+                if (isHashDifferent)
+                {
+                    _previousHash = hash;
+
+                    foreach (var terminal in terminals)
+                    {
+                        try
+                        {
+                            terminal.In.Write(new ResizeCommand
+                            {
+                                Width = bufferInfo.dwMaximumWindowSize.X,
+                                Height = bufferInfo.dwMaximumWindowSize.Y
+                            });
+                        }
+                        catch (ObjectDisposedException)
+                        {
+                            // Process has exited, next iteration should resend correct data.
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                // TODO
+                var error = PInvoke.Kernel32.GetLastError().ToString();
+                Console.WriteLine(error);
+            }
+        }
+
+        public void Dispose()
+        {
+            _sha1.Dispose();
+        }
+    }
+}
