0a1cb6d1cee419113a86578f3b6bd1efab44ce89
Sebastian Zalewski <1532036+SebZal@users.noreply.github.com>
2019-03-23 22:40:37 +0100

    Add TerminalContainer

---
+++ WinTerMul/TerminalContainer.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WinTerMul
+{
+    internal class TerminalContainer : IDisposable
+    {
+        private readonly object _lock;
+        private readonly List<Terminal> _terminals;
+
+        public TerminalContainer(params Terminal[] terminals)
+        {
+            _lock = new object();
+            _terminals = new List<Terminal>(terminals);
+        }
+
+        public Terminal ActiveTerminal { get; private set; }
+
+        public void SetNextTerminalActive()
+        {
+            lock (_lock)
+            {
+                if (_terminals.Count == 0)
+                {
+                    return;
+                }
+
+                var index = _terminals.IndexOf(ActiveTerminal) + 1;
+                if (index >= _terminals.Count)
+                {
+                    index = 0;
+                }
+                ActiveTerminal = _terminals[index];
+            }
+        }
+
+        public void SetPreviousTerminalActive()
+        {
+            lock (_lock)
+            {
+                if (_terminals.Count == 0)
+                {
+                    return;
+                }
+
+                var index = _terminals.IndexOf(ActiveTerminal) - 1;
+                if (index < 0)
+                {
+                    index = _terminals.Count - 1;
+                }
+                ActiveTerminal = _terminals[index];
+            }
+        }
+
+        public void AddTerminal(Terminal terminal)
+        {
+            lock (_lock)
+            {
+                var activeTerminalIndex = _terminals.IndexOf(ActiveTerminal);
+                ActiveTerminal = terminal;
+                _terminals.Insert(++activeTerminalIndex, ActiveTerminal);
+            }
+        }
+
+        public IReadOnlyCollection<Terminal> GetTerminals()
+        {
+            lock (_lock)
+            {
+                var terminals = new List<Terminal>();
+                Terminal previousTerminal = null;
+                var activeTerminal = ActiveTerminal;
+
+                foreach (var terminal in _terminals.ToArray())
+                {
+                    if (terminal.Process.HasExited)
+                    {
+                        if (terminal == activeTerminal)
+                        {
+                            activeTerminal = previousTerminal;
+                        }
+
+                        _terminals.Remove(terminal);
+                        terminal.Dispose();
+                    }
+                    else
+                    {
+                        if (activeTerminal == null)
+                        {
+                            activeTerminal = terminal;
+                        }
+
+                        previousTerminal = terminal;
+
+                        terminals.Add(terminal);
+                    }
+                }
+
+                ActiveTerminal = activeTerminal;
+
+                return terminals;
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var terminal in _terminals)
+            {
+                terminal.Dispose();
+            }
+            _terminals.Clear();
+        }
+    }
+}
