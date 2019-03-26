45e27812068aa281d46bf0584480c8d2f255c181
Sebastian Zalewski <1532036+SebZal@users.noreply.github.com>
2019-03-26 22:31:42 +0100

    Use NamedPipeStream instead of MemoryMappedFile

--- WinTerMul.Common/Pipe.cs
+++ WinTerMul.Common/Pipe.cs
@@ -1,127 +1,77 @@
 ﻿using System;
-using System.IO.MemoryMappedFiles;
+using System.IO.Pipes;
 using System.Security.Cryptography;
-using System.Threading;
 
 namespace WinTerMul.Common
 {
     public sealed class Pipe : IDisposable
     {
-        private const int MemorySize = 524288; // 2^19
-        private const int ChunkSize = ushort.MaxValue + 1;
-        private const byte Empty = 0;
-        private const byte NotEmpty = 1;
-
         private readonly SHA1CryptoServiceProvider _sha1;
-        private readonly MemoryMappedFile _memoryMappedFile;
 
-        private MemoryMappedViewStream _stream;
+        private PipeStream _stream;
         private byte[] _previousHash;
-        private int _currentChunk;
 
-        private Pipe(string id, MemoryMappedFile memoryMappedFile)
+        private Pipe(string id, PipeStream stream)
         {
             Id = id;
-            _memoryMappedFile = memoryMappedFile;
+            _stream = stream;
             _sha1 = new SHA1CryptoServiceProvider();
             _previousHash = new byte[_sha1.HashSize / 8];
         }
 
         public string Id { get; }
 
-        private MemoryMappedViewStream Stream => _stream ?? (_stream = _memoryMappedFile.CreateViewStream());
-
         public static Pipe Create()
         {
             var id = Guid.NewGuid().ToString();
-            var memoryMappedFile = MemoryMappedFile.CreateNew(id, MemorySize);
-            return new Pipe(id, memoryMappedFile);
+            var stream = new NamedPipeServerStream(id);
+            return new Pipe(id, stream);
         }
 
         public static Pipe Connect(string id)
         {
-            var memoryMappedFile = MemoryMappedFile.OpenExisting(id);
-            return new Pipe(id, memoryMappedFile);
+            var stream = new NamedPipeClientStream(id);
+            stream.Connect();
+            return new Pipe(id, stream);
         }
 
         public void Write(ITransferable @object, bool writeOnlyIfDataHasChanged = false)
         {
-            while (!TryWrite(@object, writeOnlyIfDataHasChanged))
+            VerifyIsConnected();
+
+            var data = Serializer.Serialize(@object);
+
+            if (writeOnlyIfDataHasChanged && !HasDataChanged(data))
             {
-                Thread.Sleep(10);
+                return;
             }
+
+            _stream.WaitForPipeDrain();
+
+            var buffer = new byte[sizeof(ushort) + data.Length];
+            Array.Copy(BitConverter.GetBytes((ushort)data.Length), 0, buffer, 0, sizeof(ushort));
+            Array.Copy(data, 0, buffer, sizeof(ushort), data.Length);
+
+            _stream.Write(buffer, 0, buffer.Length);
         }
 
         public ITransferable Read()
         {
-            var initialStreamPosition = Stream.Position;
-
-            var firstByte = Stream.ReadByte();
-            if (firstByte == Empty)
-            {
-                Stream.Position -= sizeof(byte);
-                return null;
-            }
+            VerifyIsConnected();
 
             var dataLengthBuffer = new byte[sizeof(ushort)];
-            Stream.Read(dataLengthBuffer, 0, dataLengthBuffer.Length);
+            _stream.Read(dataLengthBuffer, 0, dataLengthBuffer.Length);
             var dataLength = BitConverter.ToUInt16(dataLengthBuffer, 0);
             var data = new byte[dataLength];
-            Stream.Read(data, 0, data.Length);
-
-            // Clear first byte in order to indicate that the content has been read.
-            Stream.Position = initialStreamPosition;
-            Stream.Write(new[] { Empty }, 0, 1);
-
-            MoveStreamToNextChunk();
+            _stream.Read(data, 0, data.Length);
 
             return Serializer.Deserialize(data);
         }
 
         public void Dispose()
         {
-            _memoryMappedFile.Dispose();
-            _stream?.Dispose();
+            _stream.Dispose();
             _sha1.Dispose();
-        }
-
-        private bool TryWrite(ITransferable @object, bool writeOnlyIfDataHasChanged)
-        {
-            var initialStreamPosition = Stream.Position;
-
-            var isChunkFree = Stream.ReadByte() == Empty;
-            Stream.Position = initialStreamPosition;
-            if (!isChunkFree)
-            {
-                return false;
-            }
-
-            var data = Serializer.Serialize(@object);
-
-            if (writeOnlyIfDataHasChanged && !HasDataChanged(data))
-            {
-                return true;
-            }
-
-            var buffer = new byte[sizeof(byte) + sizeof(ushort) + data.Length];
-            if (buffer.Length > ChunkSize)
-            {
-                throw new InvalidOperationException("Data could not be written, it is exceeding chunk size.");
-            }
-
-            buffer[0] = NotEmpty;
-            Array.Copy(BitConverter.GetBytes((ushort)data.Length), 0, buffer, sizeof(byte), sizeof(ushort));
-            Array.Copy(data, 0, buffer, sizeof(byte) + sizeof(ushort), data.Length);
-
-            // Write first byte last, so the chunk is not read before everything is written.
-            Stream.Position = initialStreamPosition + sizeof(byte);
-            Stream.Write(buffer, 1, buffer.Length - 1);
-            Stream.Position = initialStreamPosition;
-            Stream.Write(buffer, 0, 1);
-
-            MoveStreamToNextChunk();
-
-            return true;
         }
 
         private bool HasDataChanged(byte[] data)
@@ -149,16 +99,12 @@ namespace WinTerMul.Common
             }
         }
 
-        private void MoveStreamToNextChunk()
+        private void VerifyIsConnected()
         {
-            var nextPosition = ++_currentChunk * ChunkSize;
-
-            if (nextPosition >= MemorySize)
+            if (_stream is NamedPipeServerStream s && !_stream.IsConnected)
             {
-                _currentChunk = nextPosition = 0;
+                s.WaitForConnection();
             }
-
-            Stream.Position = nextPosition;
         }
     }
 }
