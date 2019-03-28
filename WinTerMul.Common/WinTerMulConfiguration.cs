db20197eaf837537c3a6d8f8dc9e012f05d79586
Sebastian Zalewski <1532036+SebZal@users.noreply.github.com>
2019-03-28 14:49:14 +0100

    Include forgotten files

---
+++ WinTerMul.Common/WinTerMulConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace WinTerMul.Common
+{
+    public class WinTerMulConfiguration
+    {
+        private readonly IConfiguration _configuration;
+
+        internal WinTerMulConfiguration(IConfiguration configuration)
+        {
+            _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
+        }
+
+        public LogLevel LogLevel => (LogLevel)Enum.Parse(typeof(LogLevel), _configuration["LogLevel"]);
+    }
+}
