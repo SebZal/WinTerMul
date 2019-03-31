255304e26bb1c5d2f06fe5ec8021e4abf2e0f8ad
Sebastian Zalewski <1532036+SebZal@users.noreply.github.com>
2019-03-31 23:33:39 +0200

    Add BufferDiffelement

---
+++ WinTerMul.Common/BufferDiffElement.cs
@@ -0,0 +1,10 @@
+﻿using WinTerMul.Common.Kernel32;
+
+namespace WinTerMul.Common
+{
+    public class BufferDiffElement
+    {
+        public int Index { get; set; }
+        public CharInfo CharInfo { get; set; }
+    }
+}
